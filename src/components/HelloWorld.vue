a250c2817fc3eec82fa02b8dfa06eb94e65c3686
pandit <shrutikumaripandit@gmail.com>
2025-04-08 15:08:13 +0530

    initial commit by shruti

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <h1>Hello World {{ name }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Shruti'
+      }
+    }
+  }
+  </script>
+  
