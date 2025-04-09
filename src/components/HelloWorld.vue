32163077023c8730b0a3c2943f7e8260ed0ee851
pandit <shrutikumaripandit@gmail.com>
2025-04-09 15:21:34 +0530

    second commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
       <h1>Hello World {{ name }}</h1>
+      <img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-souvenirpixels-414612.jpg&fm=jpg " style="width: 250px; "/> <br/>
+      <img src="https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?cs=srgb&dl=pexels-souvenirpixels-414612.jpg&fm=jpg " style="width: 250px; border: 5cm; border-color: beige;"/>
+    
     </div>
   </template>
   
@@ -13,4 +16,3 @@
     }
   }
   </script>
-  
